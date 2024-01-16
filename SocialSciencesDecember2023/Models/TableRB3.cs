0ece5b7f9e150299036a7876290c8be6c4b0941f
Dmitr <141524559+Dmitri-8799@users.noreply.github.com>
2024-01-16 18:31:25 +0300

    Update TableRB3.cs

--- SocialSciencesDecember2023/Models/TableRB3.cs
+++ SocialSciencesDecember2023/Models/TableRB3.cs
@@ -6,12 +6,14 @@ namespace SocialSciencesEF2024.TableRb3
 	{
 	
         public int id { get ; set; }
+		
         public string? question { get; set; }
-		public string? option1 { get; set; }
+		
+	public string? option1 { get; set; }
         public string? option2 { get; set; }
-
-        public string? option3 { get; set; }
-		public int answerNr { get; set; }
+	public string? option3 { get; set; }
+		
+	public int answerNr { get; set; }
 
         public string? ifRight { get; set; }
         public string? ifWrong { get; set; }
